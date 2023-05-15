68b4979950c0adf4bd1c9369eea0256ce4f540c8
AlexandraP <part.s@mail.ru>
2023-05-15 13:10:28 +0300

    Remove params, edit exception

--- ShapeLibrary/DataValidation.cs
+++ ShapeLibrary/DataValidation.cs
@@ -6,40 +6,42 @@ using System.Threading.Tasks;
 
 namespace ShapeLibrary
 {
-    public static class DataValidation
+    internal static class DataValidation
     {
 
         public static List<ArgumentException> exceptions = new List<ArgumentException>()
         {
             new ArgumentException("Numerical value shoud be positive."),//case 1: <= 0
-            new ArgumentException("Incorrect values: the sum of the lengths of any two sides of a triangle is greater than the length of the remaining side."),//case 2: a + b < c
-            new ArgumentException("Too big values: calculations exceed Double.Maxvalue value.")//case 3: Math.Pow(a + b + c, x) > Double.Maxvalue
+            new ArgumentException("Too big value: exceed Double.Maxvalue value."),//case 2: Math.Pow(a + b + c, x) > Double.Maxvalue
+            new ArgumentException("Incorrect values: the sum of the lengths of any two sides of a triangle is greater than the length of the remaining side."),//case 3: a + b < c
+            new ArgumentException("Too big values: calculations exceed Double.Maxvalue value.")//case 4: Math.Pow(a + b + c, x) > Double.Maxvalue
         };
 
-        public static void BaseCheck(double value)
+        public static void CheckPozitive(double value)
         {
             if (value <= 0) throw exceptions[0]; 
         }
 
-        internal static void CheckTriangleSide(double value, double[] arr)
+        public static void CheckExceedingPossibleValue(double value)
+        {
+            if (Double.IsPositiveInfinity(Math.Pow(value, 1))) throw exceptions[1];
+        }
+
+        internal static void CheckTriangleSideSum(double value, double[] arr)
         {
             if(arr.Length == 3)
             {
-                if (arr.Sum() - 2 * value <= 0) throw exceptions[1];
+                if (arr.Sum() - 2 * value <= 0) throw exceptions[2];
             }
         }
 
-        public static void ExceedingValue(double value)
-        {
-            if (Double.IsPositiveInfinity(Math.Pow(value, 1))) throw exceptions[2];
-        }
-
-        public static void ExceedingValue(double[] arr)
+        public static void CheckExceedingTheResultForOperationsWithValues(double[] arr)
         {
             var p = arr.Sum();
-            if (Double.IsPositiveInfinity( Math.Pow(p,1))) throw exceptions[2];
-            else if (Double.IsPositiveInfinity(Math.Pow(p,2))) throw exceptions[2];
-            else if (Double.IsPositiveInfinity( Math.Pow(p,3))) throw exceptions[2];
+            for(int i = 2; i < arr.Length; i++)
+            {
+                if(Double.IsPositiveInfinity(Math.Pow(p, i))) throw exceptions[3];
+            }
         }
 
     }
--- ShapeLibraryTest/TriangleTest.cs
+++ ShapeLibraryTest/TriangleTest.cs
@@ -12,22 +12,31 @@ namespace ShapeLibraryTest
             Triangle triangle = new Triangle(2, -3, 2);
         }
 
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Too big value: exceed Double.Maxvalue value.")]
+        public void Test_DoesNot_DataValidation_CheckExceedingPossibleValue()
+        {
+            Triangle triangle = new Triangle(Double.MaxValue*10, 12, 12);
+        }
+
+
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "Incorrect values: the sum of the lengths of any two sides of a triangle is greater than the length of the remaining side.")]
-        public void Test_Not_DataValidation_CorrectLengthSide()
+        public void Test_Not_DataValidation_CorrectLengthTriangleSide()
         {
             Triangle triangle = new Triangle(2, 3, 6);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "Too big values: calculations exceed Double.Maxvalue value.")]
-        public void Test_DoesNot_DataValidation_ExceedingValue()
+        public void Test_DoesNot_DataValidation_ExceedingTheResultForOperationsWithValues()
         {
             Triangle triangle = new Triangle(1.3407807929942596E+154, 1.3407807929942596E+154, 2);
         }
 
         [TestMethod]
-        public void Test_GetArea2()
+        public void Test_GetArea()
         {
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
             Triangle triangle = new Triangle(4, 5, 6);
@@ -35,17 +44,17 @@ namespace ShapeLibraryTest
         }
 
         [TestMethod]
-        public void Test_IsRight()
+        public void Test_IsRegularTriangle()
         {
             Triangle triangle = new Triangle(3, 4, 5);
-            bool result = triangle.IsRight();
+            bool result = triangle.IsRegularTriangle();
             Assert.IsTrue(result);
         }
         [TestMethod]
-        public void Test_DoesNot_IsRight()
+        public void Test_DoesNot_IsRegularTriangle()
         {
             Triangle triangle = new Triangle(4, 5, 6);
-            bool result = triangle.IsRight();
+            bool result = triangle.IsRegularTriangle();
             Assert.IsFalse(result);
         }
 
